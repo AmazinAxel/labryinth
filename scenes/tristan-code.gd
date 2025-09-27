622778fc97de46cfe1c82dec94d6bb650e3b98e2
AmazinAxel <axel@amazinaxel.com>
2025-09-27 15:44:41 -0700

    tristan work

--- scenes/tristan-code.gd
+++ scenes/tristan-code.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+var goUp = false
+var target_y = 0
+
 func tristan():
 	print(self)
-	return
+	goUp = true
+	target_y = PlayerControls.global_position.y - 100
+
+func _physics_process(delta: float) -> void:
+	if goUp:
+		global_position.y -= 1
+		if global_position.y <= target_y:
+			goUp = false
+		print(global_position.y)
