2c93105cf34d8232175d33d512d33f5a3b049789
AmazinAxel <axel@amazinaxel.com>
2025-09-27 20:41:07 -0700

    FIX TRISTAN

--- scenes/tristan.gd
+++ scenes/tristan.gd
@@ -1,7 +1,8 @@
 extends Node2D
 
+@onready var tristan = $"../../../tristan/CharacterBody2D";
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player body":
-		TristanCode.goUp = true;
+		tristan.goUp = true;
 		#target_y = PlayerControls.global_position.y - 100
