95f02a1eb426b45356bec904967e472ab4c3b9ab
AmazinAxel <axel@amazinaxel.com>
2025-09-28 10:01:46 -0700

    feat: add tristan blink

--- scenes/tristan-code.gd
+++ scenes/tristan-code.gd
@@ -1,10 +1,7 @@
 extends CharacterBody2D
 
 @export var goUp = false
-var target_y = 0
-
 
 func _process(delta: float) -> void:
 	if goUp:
-		print("Moving up:", global_position.y)
-		global_position.y -= 1
+		global_position.y -= 5
--- scenes/tristan.gd
+++ scenes/tristan.gd
@@ -5,4 +5,14 @@ extends Node2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "player body":
 		tristan.goUp = true;
-		#target_y = PlayerControls.global_position.y - 100
+		tristan.global_position.y = PlayerControls.global_position.y + 1000
+		get_node("/root/main/tristanBlink").show()
+		await get_tree().create_timer(0.05).timeout
+		
+		get_node("/root/main/tristanBlink").hide()
+		await get_tree().create_timer(0.1).timeout
+		
+		get_node("/root/main/tristanBlink").show()
+		await get_tree().create_timer(0.2).timeout
+
+		get_node("/root/main/tristanBlink").hide()
