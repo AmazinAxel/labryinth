0a9ee20c8f0e719cfc643238621dac7bf4c62e60
AmazinAxel <axel@amazinaxel.com>
2025-09-28 13:31:01 -0700

    feat: add bob

--- node.gd
+++ node.gd
@@ -9,17 +9,17 @@ func _process(delta: float) -> void:
 	if health <= 0:
 		var deathMessage = "";
 		if lastDamageReason == "barrel":
-			deathMessage = "The barrel..?\nStaying in the barrel\nwill slowly kill you...\nGo out and back in to prevent death";
+			deathMessage = "The barrel..?\nStaying in the barrel\nwill slowly kill you...\nGo out and back in to prevent death.";
 		elif lastDamageReason == "oscar":
-			deathMessage = "Oscar got you.\nHe's not very kind to travellers like you!\nAttack him";
+			deathMessage = "Oscar got you.\nHe's not very kind to travellers like you.\nAttack him.";
 		elif lastDamageReason == "tristan":
-			deathMessage = "You died to Tristan..\nWhen you see the blinks, enter a barrel...";
+			deathMessage = "You died to Tristan..\nWhen you see the blinks,\npress F near a barrel to hide...";
 		elif lastDamageReason == "bob":
-			deathMessage = "You died to Bob..\nThey're like Tristan, but be extra careful...\nThey come back thrice!";
+			deathMessage = "You died to Bob..\nThey're like Tristan, but be extra careful...\nThey come back thrice.";
 		elif lastDamageReason == "timmy":
-			deathMessage = "Timmy got you..\nClick the spider to swat at it..\notherwise it will eat away at your health!";
+			deathMessage = "Timmy got you..\nClick the spider to swat at it..\notherwise, it'll eat your health.";
 		elif lastDamageReason == "spikes":
-			deathMessage = "The spikes..\nThe easiest hazard to avoid...\njust dont get trapped..";
+			deathMessage = "The spikes..\nStay aware..";
 		else:
 			deathMessage = "How did you die?"
 		died.emit(deathMessage)
---
+++ scenes/bob.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+@export var direction = "static"
+
+func _process(delta: float) -> void:
+	if direction == "up":
+		global_position.y -= 8
+	elif direction == "down":
+		global_position.y -= 8
