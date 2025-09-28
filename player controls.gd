69314f11d0104d593abce81020a65c6ebefdc66c
frames per second man <artemisbeams@gmail.com>
2025-09-27 19:07:08 -0700

    fsdafasd

--- player controls.gd
+++ player controls.gd
@@ -11,6 +11,9 @@ signal died
 var max_health: int = 100
 var current_health: int = max_health
 
+func _ready() -> void:
+	velocity = Vector2.UP * 800
+
 func _physics_process(delta):
 	# This line is adjusted to match the simpler action names.
 	var direction = Input.get_vector("left", "right", "up", "down")
@@ -33,6 +36,9 @@ func take_damage(amount: int):
 	if current_health <= 0:
 		died.emit()
 	# queue_free() or other death logic
+
+func _on_area_2d_body_entered(body):
+	print(body.name)
 
 func _process(delta: float) -> void:
 	current_health -= delta*.5
---
+++ scenes/oscar.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+## The speed of the monster in pixels wper second.
+@export var speed: float = 100.0
+## The distance in pixels at which the monster will detect and chase the player.
+@export var detection_range: float = 150.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var distance = global_position.distance_to(PlayerControls.global_position)
+	print(distance)
+	# --- MODIFIED LOGIC ---
+	# We check if the player is in range BUT ALSO not too close.
+	if distance < detection_range:
+	
+		var direction = (PlayerControls.global_position - global_position).normalized()
+		global_position += direction * speed * delta
